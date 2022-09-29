383935ba12abb2931fcad3063b2fd864c2fd0c99
kkqy <kuokongqingyun@gmail.com>
2022-09-30 02:20:31 +0800

    add some functions

--- asynchan.go
+++ asynchan.go
@@ -6,22 +6,22 @@ import (
 )
 
 type Asynchan struct {
-	ChanResult chan interface{}
-	ChanError  chan error
-	Ctx        context.Context
+	ctx        context.Context
+	chanResult chan interface{}
+	chanError  chan error
 }
 
 func (p *Asynchan) AwaitOne() (interface{}, error) {
 	select {
-	case result, ok := <-p.ChanResult:
+	case result, ok := <-p.chanResult:
 		if ok {
 			return result, nil
 		} else {
 			return nil, errors.New("no result")
 		}
-	case err := <-p.ChanError:
+	case err := <-p.chanError:
 		return nil, err
-	case <-p.Ctx.Done():
+	case <-p.ctx.Done():
 		return nil, errors.New("cancelled")
 	}
 }
@@ -29,25 +29,44 @@ func (p *Asynchan) AwaitAll() ([]interface{}, error) {
 	results := make([]interface{}, 0)
 	for {
 		select {
-		case result, ok := <-p.ChanResult:
+		case result, ok := <-p.chanResult:
 			if ok {
 				results = append(results, result)
 			} else {
 				return results, nil
 			}
-		case err := <-p.ChanError:
+		case err := <-p.chanError:
 			return results, err
-		case <-p.Ctx.Done():
+		case <-p.ctx.Done():
 			return results, errors.New("timeout")
 		}
 	}
 }
-
+func (p *Asynchan) SetResult(result interface{}) error {
+	select {
+	case p.chanResult <- result:
+		return nil
+	case <-p.ctx.Done():
+		return errors.New("cancelled")
+	}
+}
+func (p *Asynchan) SetError(err error) error {
+	select {
+	case p.chanError <- err:
+		return nil
+	case <-p.ctx.Done():
+		return errors.New("cancelled")
+	}
+}
+func (p *Asynchan) Close(err error) {
+	close(p.chanResult)
+	close(p.chanError)
+}
 func New(ctx context.Context) *Asynchan {
 	p := &Asynchan{
-		ChanResult: make(chan interface{}),
-		ChanError:  make(chan error),
-		Ctx:        ctx,
+		chanResult: make(chan interface{}),
+		chanError:  make(chan error),
+		ctx:        ctx,
 	}
 	return p
 }
